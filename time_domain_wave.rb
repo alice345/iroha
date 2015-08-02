ad73cef2a98422ae1e6b29c72f5a2279fccc8738
alice345 <alice.maru345@gmail.com>
2015-08-02 20:20:26 +0900

    タイムドメインの波形をCSVに吐くようなスクリプト

--- time_domain_wave.rb
+++ time_domain_wave.rb
@@ -1,9 +1,20 @@
+require 'csv'
+require 'pry'
 require_relative 'chunk'
 
 file = File.open(ARGV[0], mode = 'rb')
+csv = CSV.open('time_domain_wave.csv', mode = 'w')
 chunk = FormAiffChunk.new(file)
 common = CommonChunk.new(chunk)
-sound_data = SoundDataChunk.new(chunk)
+sound_chunk = SoundDataChunk.new(chunk)
 
-puts chunk.to_s, common.to_s, sound_data.to_s
+output = Array.new
+puts chunk.to_s, common.to_s, sound_chunk.to_s
+
+common.num_sample_frames.times do |frame|
+  csv << [frame.to_f/common.sample_rate, sound_chunk.data.slice!(0,4).slice(0,2).unpack('n')[0].to_i]
+  puts "進捗#{frame/common.num_sample_frames*100}%"
+end
+
 file.close
+csv.close
